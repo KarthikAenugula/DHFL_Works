6fd93d0ecdcb2d2cede950a7e5450bb684c303cd
KarthikAenugula <aenugula.karthik@gmail.com>
2020-10-12 16:55:43 +0530

    Initial Service
    Initial Service

---
+++ FieldValidation/src/main/java/com/validation/FieldValidation/FieldValidationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.validation.FieldValidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FieldValidationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FieldValidationServiceApplication.class, args);
+	}
+
+}
---
+++ FieldValidation/src/test/java/com/validation/FieldValidation/FieldValidationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.validation.FieldValidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FieldValidationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
