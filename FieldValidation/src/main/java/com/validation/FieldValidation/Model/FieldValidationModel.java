a408eedc56c1a8ceffaf9f970ca2fe290f4b4502
KarthikAenugula <aenugula.karthik@gmail.com>
2020-10-13 21:18:05 +0530

    excel reading
    excel reading

---
+++ FieldValidation/src/main/java/com/validation/FieldValidation/Model/FieldValidationModel.java
@@ -0,0 +1,163 @@
+package com.validation.FieldValidation.Model;
+
+public class FieldValidationModel {
+
+	public String brLoanCode;
+	public String applicationNo;
+	public String customerName;
+	public String mobileNo;
+	public String overdueEMI;
+	public String totaloverdueEMI;
+	public String minimumOverdueAmount;
+	public String overDueBlankField;
+	public String charges;
+	public String totalChargesAmount;
+	public String minimumChargeAmount;
+	public String chargeBlankField;
+	/**
+	 * @return the brLoanCode
+	 */
+	public String getBrLoanCode() {
+		return brLoanCode;
+	}
+	/**
+	 * @param brLoanCode the brLoanCode to set
+	 */
+	public void setBrLoanCode(String brLoanCode) {
+		this.brLoanCode = brLoanCode;
+	}
+	/**
+	 * @return the applicationNo
+	 */
+	public String getApplicationNo() {
+		return applicationNo;
+	}
+	/**
+	 * @param applicationNo the applicationNo to set
+	 */
+	public void setApplicationNo(String applicationNo) {
+		this.applicationNo = applicationNo;
+	}
+	/**
+	 * @return the customerName
+	 */
+	public String getCustomerName() {
+		return customerName;
+	}
+	/**
+	 * @param customerName the customerName to set
+	 */
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	/**
+	 * @return the mobileNo
+	 */
+	public String getMobileNo() {
+		return mobileNo;
+	}
+	/**
+	 * @param mobileNo the mobileNo to set
+	 */
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	/**
+	 * @return the overdueEMI
+	 */
+	public String getOverdueEMI() {
+		return overdueEMI;
+	}
+	/**
+	 * @param overdueEMI the overdueEMI to set
+	 */
+	public void setOverdueEMI(String overdueEMI) {
+		this.overdueEMI = overdueEMI;
+	}
+	/**
+	 * @return the totaloverdueEMI
+	 */
+	public String getTotaloverdueEMI() {
+		return totaloverdueEMI;
+	}
+	/**
+	 * @param totaloverdueEMI the totaloverdueEMI to set
+	 */
+	public void setTotaloverdueEMI(String totaloverdueEMI) {
+		this.totaloverdueEMI = totaloverdueEMI;
+	}
+	/**
+	 * @return the minimumOverdueAmount
+	 */
+	public String getMinimumOverdueAmount() {
+		return minimumOverdueAmount;
+	}
+	/**
+	 * @param minimumOverdueAmount the minimumOverdueAmount to set
+	 */
+	public void setMinimumOverdueAmount(String minimumOverdueAmount) {
+		this.minimumOverdueAmount = minimumOverdueAmount;
+	}
+	/**
+	 * @return the overDueBlankField
+	 */
+	public String getOverDueBlankField() {
+		return overDueBlankField;
+	}
+	/**
+	 * @param overDueBlankField the overDueBlankField to set
+	 */
+	public void setOverDueBlankField(String overDueBlankField) {
+		this.overDueBlankField = overDueBlankField;
+	}
+	/**
+	 * @return the charges
+	 */
+	public String getCharges() {
+		return charges;
+	}
+	/**
+	 * @param charges the charges to set
+	 */
+	public void setCharges(String charges) {
+		this.charges = charges;
+	}
+	/**
+	 * @return the totalChargesAmount
+	 */
+	public String getTotalChargesAmount() {
+		return totalChargesAmount;
+	}
+	/**
+	 * @param totalChargesAmount the totalChargesAmount to set
+	 */
+	public void setTotalChargesAmount(String totalChargesAmount) {
+		this.totalChargesAmount = totalChargesAmount;
+	}
+	/**
+	 * @return the minimumChargeAmount
+	 */
+	public String getMinimumChargeAmount() {
+		return minimumChargeAmount;
+	}
+	/**
+	 * @param minimumChargeAmount the minimumChargeAmount to set
+	 */
+	public void setMinimumChargeAmount(String minimumChargeAmount) {
+		this.minimumChargeAmount = minimumChargeAmount;
+	}
+	/**
+	 * @return the chargeBlankField
+	 */
+	public String getChargeBlankField() {
+		return chargeBlankField;
+	}
+	/**
+	 * @param chargeBlankField the chargeBlankField to set
+	 */
+	public void setChargeBlankField(String chargeBlankField) {
+		this.chargeBlankField = chargeBlankField;
+	}
+	
+
+}
--- FieldValidation/src/main/java/com/validation/FieldValidation/Service/FieldValidationServiceImpl.java
+++ FieldValidation/src/main/java/com/validation/FieldValidation/Service/FieldValidationServiceImpl.java
@@ -3,15 +3,21 @@
  */
 package com.validation.FieldValidation.Service;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
+
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
+
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Component;
+
+import com.validation.FieldValidation.Model.FieldValidationModel;
 
 /**
  * @author Karthik
@@ -20,38 +26,129 @@ import org.springframework.stereotype.Component;
 @Component
 public class FieldValidationServiceImpl {
 
-	@SuppressWarnings("deprecation")
 	public void readFile() {
 
 		try {
 
-			File file = new File("E:\\Click_To_Pay_Fields_15_06_2020-edited.xlsx"); // creating a new file instance
-			FileInputStream fis = new FileInputStream(file); // obtaining bytes from the file
-			// creating Workbook instance that refers to .xlsx file
-			XSSFWorkbook wb = new XSSFWorkbook(fis);
-			XSSFSheet sheet = wb.getSheetAt(0); // creating a Sheet object to retrieve object
-			Iterator<Row> itr = sheet.iterator(); // iterating over excel file
-			while (itr.hasNext()) {
-				Row row = itr.next();
-				Iterator<Cell> cellIterator = row.cellIterator(); // iterating over each column
-				while (cellIterator.hasNext()) {
-					Cell cell = cellIterator.next();
-					switch (cell.getCellType()) {
-					case Cell.CELL_TYPE_STRING: // field that represents string cell type
-						System.out.print(cell.getStringCellValue() + "\t\t\t");
-						break;
-					case Cell.CELL_TYPE_NUMERIC: // field that represents number cell type
-						System.out.print(cell.getNumericCellValue() + "\t\t\t");
-						break;
-					default:
-					}
+			File excel = new File("E://Click_To_Pay_Fields_15_06_2020-edited.xlsx");
+
+			FileInputStream fis = new FileInputStream(excel);
+
+			XSSFWorkbook book = new XSSFWorkbook(fis);
+			XSSFSheet sheet = book.getSheetAt(0);
+			// System.out.println("last row num=" + sheet.getLastRowNum());
+
+			Iterator<Row> iterator = sheet.iterator(); // Iterating over Excel file in Java
+
+			List<FieldValidationModel> excelList = new ArrayList<>();
+
+			while (iterator.hasNext()) {
+
+				Row nextRow = iterator.next();
+
+				// System.out.println("nextRow=" + nextRow.getRowNum());
+
+				if (nextRow.getRowNum() == 0) {
+					continue;
 				}
-				System.out.println("");
+
+				FieldValidationModel model = new FieldValidationModel();
+				if (null == nextRow.getCell(0)) {
+					model.setBrLoanCode(null);
+				} else {
+					model.setBrLoanCode(nextRow.getCell(0).toString());
+				}
+
+				if (null == nextRow.getCell(1)) {
+					model.setApplicationNo(null);
+				} else {
+					model.setApplicationNo(nextRow.getCell(01).toString());
+				}
+
+				if (null == nextRow.getCell(2)) {
+					model.setCustomerName(null);
+				} else {
+					model.setCustomerName(nextRow.getCell(2).toString());
+				}
+
+				if (null == nextRow.getCell(3)) {
+					model.setMobileNo(null);
+				} else {
+					model.setMobileNo(nextRow.getCell(3).toString());
+				}
+
+				if (null == nextRow.getCell(4)) {
+					model.setOverdueEMI(null);
+				} else {
+					model.setOverdueEMI(nextRow.getCell(4).toString());
+				}
+
+				if (null == nextRow.getCell(5)) {
+					model.setTotaloverdueEMI(null);
+				} else {
+					model.setTotaloverdueEMI(nextRow.getCell(5).toString());
+				}
+
+				if (null == nextRow.getCell(6)) {
+					model.setMinimumOverdueAmount(null);
+				} else {
+					model.setMinimumOverdueAmount(nextRow.getCell(6).toString());
+				}
+
+				if (null == nextRow.getCell(7)) {
+					model.setOverDueBlankField(null);
+				} else {
+					model.setOverDueBlankField(nextRow.getCell(7).toString());
+				}
+
+				if (null == nextRow.getCell(8)) {
+					model.setCharges(null);
+				} else {
+					model.setCharges(nextRow.getCell(8).toString());
+				}
+
+				if (null == nextRow.getCell(9)) {
+					model.setTotalChargesAmount(null);
+				} else {
+					model.setTotalChargesAmount(nextRow.getCell(9).toString());
+				}
+
+				if (null == nextRow.getCell(10)) {
+					model.setMinimumChargeAmount(null);
+				} else {
+					model.setMinimumChargeAmount(nextRow.getCell(10).toString());
+				}
+
+				if (null == nextRow.getCell(11)) {
+					model.setChargeBlankField(null);
+				} else {
+					model.setChargeBlankField(nextRow.getCell(11).toString());
+				}
+
+				excelList.add(model);
+
+				System.out.println("excelList size=" + excelList.size());
 			}
+			book.close();
+			fis.close();
+
+			int value = validationMethod(excelList);
+
+		} catch (NullPointerException e) {
+			e.printStackTrace();
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
+	private int validationMethod(List<FieldValidationModel> excelList) {
+
+		try {
+
+		} catch (Exception e) {
+
+		}
+
+		return 0;
+	}
 }
